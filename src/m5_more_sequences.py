6a826f98e4c3fbcc54d2fb4bca4a7fb296800a24
Ryan Walter <walterrm@rose-hulman.edu>
2018-04-02 00:09:28 -0400

    Done

--- src/m5_more_sequences.py
+++ src/m5_more_sequences.py
@@ -312,7 +312,7 @@ def contains_an_a(s):
     #   Use an explicit loop, as you have done in the other problems.
     #   No fair using the   count   or   find   string methods.
     # ------------------------------------------------------------------
-    
+
     for k in s:
         if k == 'a':
             return True
